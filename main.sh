84384ff1f07f77f462a15587cf884b3730095188
josephine324 <j.mutesi@alustudent.com>
2023-06-25 08:15:37 -0700

    updated group work

--- main.sh
+++ main.sh
@@ -1,79 +1,60 @@
 #!/usr/bin/env bash
-#building an application
-students_file="students-list_0333.txt"
 
-create_student() {
-    read -p "Enter student email: " email
-    read -p "Enter student age: " age
-    read -p "Enter student ID: " student_id
-
-    echo "$email $age $student_id" >> "$students_file"
-    echo "Student record created."
-}
-
-view_students() {
-	if [ -s "$students_file" ]; then
-		cat $students_file
-	else
-		echo "No students found."
-	fi
-}
-
-delete_student() {
-    read -p "Enter student ID to delete: " student_id
-    if grep -q "^.* $student_id\$" "$students_file"; then
-        sed -i "/^.* $student_id\$/d" "$students_file"
-        echo "Student deleted."
-    else
-        echo "Student not found."
-    fi
-}
-update_student() {
-    read -p "Enter student ID to update: " student_id
-    if grep -q "^.* $student_id\$" "$students_file"; then
-        read -p "Enter updated email: " email
-        read -p "Enter updated age: " age
-
-        sed -i "/^.* $student_id\$/d" "$students_file"
-        echo "$email $age $student_id" >> "$students_file"
-        echo "Student updated."
-    else
-        echo "Student not found."
-    fi
-}
-
-while true
-do
-    echo "----- Bachelor of Software Engineering Cohort List -----"
+while true; do
+    echo "========================"
+    echo "Bachelor of Software Engineering Cohort List"
+    echo "========================"
     echo "1. Create student record"
     echo "2. View all students"
-    echo "3. Delete student"
+    echo "3. Delete student record"
     echo "4. Update student record"
     echo "5. Exit"
-
-    read -p "Enter your choice (1-5): " choice
+    echo "========================"
+    echo -n "Enter your choice (1-5): "
+    read choice
 
     case $choice in
-        1)
-            create_student
+        1) # Create student record
+            echo "Enter student email: "
+            read email
+            echo "Enter student age: "
+            read age
+            echo "Enter student ID: "
+            read id
+
+            echo "$email,$age,$id" >> students-list_0333.txt
+            echo "Student record created successfully!"
             ;;
-        2)
-            view_students
+        2) # View all students
+            echo "========================"
+            echo "Student List"
+            echo "========================"
+            cat students-list_0333.txt
             ;;
-        3)
-            delete_student
+        3) # Delete student record
+            echo -n "Enter student ID to delete: "
+            read delete_id
+            sed -i "/$delete_id/d" students-list_0333.txt
+            echo "Student record deleted successfully!"
             ;;
-        4)
-            update_student
+        4) # Update student record
+            echo -n "Enter student ID to update: "
+            read update_id
+            echo -n "Enter updated email: "
+            read updated_email
+            echo -n "Enter updated age: "
+            read updated_age
+
+            sed -i "s/$update_id.*,/$updated_email,$updated_age,$update_id/" students-list_0333.txt
+            echo "Student record updated successfully!"
             ;;
-        5)
-            echo "Exiting the application."
-            exit
+        5) # Exit the application
+            echo "Exiting the application..."
+            exit 0
             ;;
-        *)
-            echo "Invalid choice. Please enter a number from 1 to 5."
+        *) # Invalid choice
+            echo "Invalid choice. Please enter a number between 1 and 5."
             ;;
     esac
-
-    echo "-----------------------------------------------------"
 done
+
--- select-emails.sh
+++ select-emails.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-#Create a command to select just the emails of the student.
-filename="students-list_0333.txt"
-output="student-emails.txt"
 
-awk -F ', ' '{ print $2 }' $filename > $output
+grep -oE '^\w+@\w+\.\w+' students-list_0333.txt > student-emails.txt
+
